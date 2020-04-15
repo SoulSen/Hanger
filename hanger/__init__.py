49225223c24ee8ee89279ab72e0d73cecec38b8f
SoulSen <talukdershowmen@gmail.com>
2020-04-14 18:28:28 -0700

    Update Version

--- hanger/__init__.py
+++ hanger/__init__.py
@@ -1,11 +1,10 @@
 from .client import Client
-from .message import Message
+from .conversation import Conversation
 from .image import Image
 from .location import Location
-from .conversation import Conversation
-
+from .message import Message
 
 __title__ = 'hanger'
 __author__ = 'SoulSen'
 __license__ = 'MIT'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
